d61de1ba1d3cba283a38c792b6f52d8b125cd106
Vandana Yadav <67888275+vandu565@users.noreply.github.com>
2020-11-30 11:28:34 +0530

    Update calci.js
    final commit

--- calci.js
+++ calci.js
@@ -1,75 +1,61 @@
-var buttons=document.getElementsByClassName("button");
-var display=document.getElementById("display");
-var operand1=0;
-var operand2=null;
-var operator=null;
+
+var buttons = document.getElementsByClassName("button");
+var display = document.getElementById("display");
 
 
-for(var i=0; i<buttons.length;i++)
-{
-    buttons[i].addEventListener('click',function(){
-        
-        var value= this.getAttribute('data-value');
-        if(value=='+'){
-            operator='+';
-            operand1=parseFloat(display.textContent);
-            display.textContent = "";
-        }
-        else if(value=='%'){
-            operator='%';
-            operand1=parseFloat(display.textContent);
-            display.textContent = "";
-        }
-        else if(value=='/'){
-            operator='/';
-            operand1=parseFloat(display.textContent);
-            display.textContent = "";
-        }
-        else if(value=='-'){
-            operator='-';
-            operand1=parseFloat(display.textContent);
-            display.textContent = "";
-        }
-        else if(value=='*'){
-            operator='*';
-            operand1=parseFloat(display.textContent);
-            display.textContent = "";
-        }
-        else if (value == "sign") {
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
+
+function isOperator(value) {
+    return value == "+" || value == "-" || value == "*" || value == "/" || value=="%";
+}
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function () {
+
+        var value = this.getAttribute('data-value');
+      
+        var text = display.textContent.trim();
+
+        if (isOperator(value)) {
+            operator = value;
             operand1 = parseFloat(text);
+            display.textContent += value;
+        } else if (value == "AC") {
+            display.textContent = "";
+        } else if (value == "sign") {
+           operand1 = parseFloat(text);
             operand1 = -1 * operand1;
-            display.textContent = operand1;}
-        
-        
-        
-        
-        else if(value=='='){
-            operand2=parseFloat(display.textContent);
-            var result=eval(operand1 +" "+ operator+" " + operand2);
-            if (result) {
+           display.textContent = operand1;
+        } else if (value == ".") {
+            if (text.length && !text.includes('.')) {
+                display.textContent = text + '.';
+            }
+        } else if (value == "%") {
+            operand1 = parseFloat(text);
+            operand1 = operand1 / 100;
+           display.textContent += value;
+        } else if (value == "=") {
+            var x=display.textContent;
+           var posi =x.indexOf(operator);
+            var s=operator;
+            var len=s.length;
+            operand2=parseFloat(x.slice(posi+len, x.length));
+            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+            if (result || result==0) {
                 display.textContent = result;
                 operand1 = result;
                 operand2 = null;
                 operator = null;
             }
-
         }
-        else if(value=='AC'){
-            display.textContent = "";
-
-
-        }
-        
-        
-        else{
+        else {
             display.textContent += value;
         }
-
-
-
-
-
-
-
     });
 }
+
+
+
+
